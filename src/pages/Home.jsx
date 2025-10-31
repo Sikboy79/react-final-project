058ad9ea34106613f4ce0878d5985418e255f6b6
Sikboy79 <custombasewelds@yahoo.com>
2025-10-31 09:30:11 -0600

    enviroment setup

---
+++ src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Header from '../components/Header'
+
+const Home = () => {
+  return (
+    <>
+    <Header>
+
+    </Header>
+      <div className='body'>
+        <div className='search-cards'>
+          <div className='search-card'></div>
+          <div className='search-card'></div>
+          <div className='search-card'></div>
+          <div className='search-card'></div>
+          <div className='search-card'></div>
+          <div className='search-card'></div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Home
